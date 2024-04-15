e2c778e4dcebd111e636c86b4cf878088b8a01af
Hunter <hunterde91@gmail.com>
2024-04-15 18:50:38 -0400

    the get weather function is now pulling values from the coords function

--- assets/scripts.js
+++ assets/scripts.js
@@ -4,39 +4,47 @@ const cityInput = document.querySelector('#cityName')
 //form submit handler
 const formSubmitHandler = function (event) {
     event.preventDefault();
-  
+
     const city = cityInput.value.trim();
     console.log(city);
 
-    getgetCityCoords(city);
-    }
-    //   repoContainerEl.textContent = '';
-    //   cityInput.value = '';
-    // } else {
-    //   alert('Please enter a valid City');
-    // };
+    getCityCoords(city);
+}
+//   repoContainerEl.textContent = '';
+//   cityInput.value = '';
+// } else {
+//   alert('Please enter a valid City');
+// };
 // //fetch weather api data
 // //fetch command to openweatherAI, api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-const getWeather = function () {
-    let coords = getCityCoords(coords)
+const getWeather = function (coords) {
     console.log(coords);
-}
+    const apiUrl = `api.openweathermap.org/data/2.5/forecast?lat=${coords.lat}&lon=${coords.lon}&appid=f0bf82795e5d7afe9c785c5b5e558533`
+    console.log(apiUrl);
+    fetch(apiUrl)
+        
+
+       
 
 // //another fetch command to get city lon and lat, http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 const getCityCoords = function (city) {
     const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=f0bf82795e5d7afe9c785c5b5e558533`
 
     fetch(apiUrl)
-    .then(function (response) {
-        if (response.ok) {
-          response.json().then(function (data) {
-            for (let i = 0; i < data.length; i++) {
-                const coords = {
-                    lat:(data[i].lat),
-                    lon:(data[i].lon)
-                }
-                return coords;
-          }})}})};
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    for (let i = 0; i < data.length; i++) {
+                        const coords = {
+                            lat: (data[i].lat),
+                            lon: (data[i].lon)
+                        }
+                        getWeather(coords);
+                    }
+                })
+            }
+        })
+};
 
 //function to display error message if city not found
 //take data and create new elements
